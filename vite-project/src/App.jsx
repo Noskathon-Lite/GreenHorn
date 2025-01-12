5d943ea5dc7cbcfd58a1313f9fac5005aaf17844
arkhosbijit <l.bijit07@gmail.com>
2025-01-12 10:35:26 +0545

    actual start

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// import HomePage from "./pages/HomePage";
-// import Profile from "./pages/Profile";
-// import Contributions from "./pages/Contributions";
-// import AboutUs from "./pages/AboutUs";
-
 function App() {
   return (
-        // <BrowserRouter>
-        //   <Routes>
-        //     <Route index element={<HomePage />} />
-        //     <Route path="contributions" element={<Contributions />} />
-        //     <Route path="aboutUs" element={<AboutUs />} />
-        //     <Route path="profile" element={<Profile />} />
-        //   </Routes>
-        // </BrowserRouter>
         <div>
           Hello BISHAL
              
