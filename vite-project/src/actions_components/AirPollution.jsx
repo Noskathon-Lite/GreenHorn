3c26337049b8339f6aa0d76f013ef571502a875f
giddybishal <nbishal7@gmail.com>
2025-01-12 16:33:26 +0545

    beginning first air activity

---
+++ vite-project/src/actions_components/AirPollution.jsx
@@ -0,0 +1,30 @@
+import { useWeather } from '../contexts/WeatherDataProvider'
+import styles from './AirPollution.module.css'
+import AirActivities1 from '../activities/AirActivities1'
+
+function AirPollution() {
+  const { airIndex } = useWeather();
+
+  if(!airIndex) return;
+
+  if( airIndex < 3){
+    return(
+      <div className={styles.airMain}>
+        <p className={`${styles.heading} ${styles.good}`}>Air quality index of <strong>{airIndex}</strong> is very good. Let's work on something more relevant<span>🤝</span></p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.airMain}>
+
+      <p className={`${styles.heading} ${styles.bad}`}>Air quality index of <strong>{airIndex}</strong> is very bad. Let's help improve that<span>🤝</span></p>
+
+      <div className={styles.actionBox}>
+        <AirActivities1 />
+      </div>
+    </div>
+  )
+}
+
+export default AirPollution
--- vite-project/src/pages/HomePage.jsx
+++ vite-project/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
-// import AirPollution from "../actions_components/AirPollution"
 import NavBar from "../components/NavBar"
 import WeatherReport from "../components/WeatherReport"
+import AirPollution from "../actions_components/AirPollution"
 
 function HomePage() {
   return (
     <div>
       <NavBar />
       <WeatherReport />
-      {/* <AirPollution /> */}
+      <AirPollution />
     </div>
   )
 }
